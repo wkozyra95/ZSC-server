1ae6f0d0a16b67e87956bf46e28df19a4ca3b6d5
Unknown <krzychuwr1@gmail.com>
2016-12-03 23:31:22 +0100

    Add checksum calculation method

--- Utils.cpp
+++ Utils.cpp
@@ -20,3 +20,28 @@ std::string Utils::decimal_format_display(uint8_t *data, ssize_t size) {
     delete[] dataDisplay;
     return str;
 }
+
+uint16_t checksum (uint8_t data[], int len)
+{
+    uint16_t *addr = (uint16_t*)data;
+    int len16 = (len + 1) / 2;
+    uint32_t sum = 0;
+    uint16_t result = 0;
+
+    while (len16 > 1) {
+        sum += *(addr++);
+        len16 -= 2;
+    }
+
+    if (len16 > 0) {
+        sum += *(uint8_t *) addr;
+    }
+
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+
+    result = ~sum;
+
+    return (result);
+}
